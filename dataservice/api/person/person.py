9310725d6cb3fba96c68b472b95899bb49df71d1
Natasha Singh <dukedesi22@gmail.com>
2018-01-11 17:01:03 -0500

    🔧 Update person api with all crud methods
    Implement post to create one, put to update one, get to read one
    get to read all and delete to delete one

--- dataservice/api/person/person.py
+++ dataservice/api/person/person.py
@@ -26,8 +26,12 @@ person_model = person_api.model('Person', {
         description='Identifier used in the original study data')
 })
 
+person_list = person_api.model("Persons", {
+        "persons": fields.List(fields.Nested(person_model))
+})
+
 response_model = person_api.model('Response', {
-    'content': fields.Nested(person_model),
+    'content': fields.Nested(person_list),
     'status': fields.Integer(
         description='HTTP response status code',
         example=200),
@@ -37,13 +41,26 @@ response_model = person_api.model('Response', {
 })
 
 
-@person_api.route('/<string:person_id>')
-class Person(Resource):
+@person_api.route('/')
+class Persons(Resource):
+    @person_api.marshal_with(response_model)
+    def get(self):
+        """
+        Get all persons
+        """
+        persons = model.Person.query.all()
+        return {'status': 200,
+                'message': '{} persons'.format(len(persons)),
+                'content': {'persons': persons}}, 200
+
+
+@person_api.route('/')
+class NewPerson(Resource):
     @person_api.marshal_with(response_model)
     @person_api.doc(responses={201: 'person created',
                                400: 'invalid data'})
     @person_api.expect(person_model)
-    def put(self, person_id):
+    def post(self):
         """
         Create a new person
 
@@ -55,28 +72,54 @@ class Person(Resource):
         db.session.commit()
         return {'status': 201,
                 'message': 'person created',
-                'content': person}, 201
+                'content': {'persons':[person]}}, 201
 
+
+@person_api.route('/<string:person_id>')
+class ExistingPerson(Resource):
     @person_api.marshal_with(response_model)
     @person_api.doc(responses={200: 'person found',
                                404: 'person not found'})
     def get(self, person_id):
         """
         Get a person by id
-
         Gets a person given a Kids First id
         """
-        person = model.Person.query.get_or_404(person_id)
+        person = model.Person.query.filter_by(kf_id=person_id).first_or_404()
         return {'status': 200,
                 'message': 'person found',
-                'content': person}, 200
+                'content': {'persons':[person]}}, 200
 
+    @person_api.marshal_with(response_model)
+    @person_api.doc(responses={201: 'person updated',
+                               400: 'invalid data',
+                               404: 'person not found'})
+    @person_api.expect(person_model)
+    def put(self, person_id):
+        """
+        Update an existing person
+        """
+        body = request.json
+        person = model.Person.query.filter_by(kf_id=person_id).first_or_404()
+        person.source_name = body.get('source_name')
+        db.session.commit()
+
+        return {'status': 201,
+                'message': 'person updated',
+                'content': {'persons':[person]}}, 201
+
+    @person_api.marshal_with(response_model)
+    @person_api.doc(responses={204: 'person deleted',
+                               404: 'person not found'})
     def delete(self, person_id):
         """
         Delete person by id
 
         Deletes a person given a Kids First id
         """
-        person = model.Person.query.get_or_404(person_id)
+        person = model.Person.query.filter_by(kf_id=person_id).first_or_404()
+        db.session.delete(person)
+        db.session.commit()
         return {'status': 200,
-                'message': 'deleted'}, 200
+                'message': 'person deleted',
+                'content': {'persons':[person]}}, 200
