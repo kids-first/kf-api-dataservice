4a6a1ecd2b6f2df162a941df23bb5590bfcd6180
Dan Kolbman <dankolbman@gmail.com>
2018-01-19 14:46:27 -0500

    :sparkles: Adds status page and reorgs routing

--- dataservice/api/__init__.py
+++ dataservice/api/__init__.py
@@ -1,17 +1,48 @@
+import pkg_resources
+import subprocess
 from flask import Blueprint
 from flask_restplus import Api
 from dataservice.api.participant import participant_api
 
-api_v1 = Blueprint('api', __name__, url_prefix='/v1')
+api_v1 = Blueprint('api', __name__, url_prefix='')
 
 api = Api(api_v1,
           title='Kids First Data Service',
           description=open('dataservice/api/README.md').read(),
-          version='0.1',
+          version=_get_version(),
           default='',
           default_label='')
 
 api.add_namespace(participant_api)
+
+
+@api.route("/status")
+class Status(Resource):
+    """
+    Service Status
+    """
+    @api.marshal_with(version_response)
+    def get(self):
+        """
+        Get the service status
+
+        Returns information about the current API's version and status
+        """
+        commit = (subprocess.check_output(
+                  ["git", "rev-parse", "--short", "HEAD"])
+                  .decode("utf-8").strip())
+
+        tags = (subprocess.check_output(
+                ["git", "tag", "-l", "--points-at", "HEAD"])
+                .decode("utf-8").split('\n'))
+        tags = [] if tags[0] == "" else tags
+        return {"_status": {
+                    "message": "Welcome to the Kids First Dataservice API",
+                    "code": 200,
+                    "version": _get_version(),
+                    "commit": commit,
+                    "tags": tags
+                }}
 
 
 @api.documentation
--- dataservice/api/common/serializers.py
+++ dataservice/api/common/serializers.py
@@ -44,3 +44,20 @@ _paginate_fields = Model('PaginateFields', {
 base_pagination = base_response.clone('BasePagination', {
     '_links': fields.Nested(_paginate_fields)
 })
+
+_version_fields = _status_fields.clone('VersionFields', {
+    'version': fields.String(
+        example='/persons?page=3',
+        description='Location of the next page'),
+    'commit': fields.String(
+        example='23cf525',
+        description='Commit currently deployed'),
+    'tags': fields.List(fields.String(
+        example='rc-0.8.4',
+        description='Any tags on the current commit'))
+})
+
+# Version response model
+version_response = base_response.clone('VersionResponse', {
+    '_status': fields.Nested(_version_fields)
+})
