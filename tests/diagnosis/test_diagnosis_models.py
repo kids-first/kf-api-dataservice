242945e78999d57f845ef2545a7b0c6165e21993
Natasha Singh <dukedesi22@gmail.com>
2018-01-24 15:56:32 -0500

    :white_check_mark: Update tests to test one-to-many relationship

--- tests/diagnosis/test_diagnosis_models.py
+++ tests/diagnosis/test_diagnosis_models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import uuid
 
+from sqlalchemy.exc import IntegrityError
+
 from dataservice.api.participant.models import Participant
 from dataservice.api.diagnosis.models import Diagnosis
 from dataservice.extensions import db
@@ -16,18 +18,25 @@ class ModelTest(FlaskTestCase):
         """
         Test create diagnosis
         """
+        # Create and save participant
+        participant_id = 'Test subject 0'
+        p = Participant(external_id=participant_id)
+        db.session.add(p)
+        db.session.commit()
+
         # Create diagnoses
         data = {
             'external_id': 'diag_1',
-            'primary_diagnosis': 'cold',
-            'progression_or_recurence': 'yes',
-            'age_at_event_days': 120
+            'diagnosis': 'cold',
+            'progression_or_recurrence': 'yes',
+            'age_at_event_days': 120,
+            'participant_id': p.kf_id
         }
         dt = datetime.now()
         d1 = Diagnosis(**data)
         db.session.add(d1)
         data['external_id'] = 'diag_2'
-        data['primary_diagnosis'] = 'flu'
+        data['diagnosis'] = 'flu'
         d2 = Diagnosis(**data)
         db.session.add(d2)
         db.session.commit()
@@ -39,10 +48,10 @@ class ModelTest(FlaskTestCase):
         self.assertIs(type(uuid.UUID(new_diagnosis.uuid)), uuid.UUID)
 
         self.assertEqual(new_diagnosis.external_id, data['external_id'])
-        self.assertEqual(new_diagnosis.primary_diagnosis,
-                         data['primary_diagnosis'])
-        self.assertEqual(new_diagnosis.progression_or_recurence,
-                         data['progression_or_recurence'])
+        self.assertEqual(new_diagnosis.diagnosis,
+                         data['diagnosis'])
+        self.assertEqual(new_diagnosis.progression_or_recurrence,
+                         data['progression_or_recurrence'])
 
     def test_create_via_participant(self):
         """
@@ -50,8 +59,8 @@ class ModelTest(FlaskTestCase):
         """
         # Create two diagnoses
         pd = ['cold', 'flu']
-        d1 = Diagnosis(primary_diagnosis=pd[0])
-        d2 = Diagnosis(primary_diagnosis=pd[1])
+        d1 = Diagnosis(diagnosis=pd[0])
+        d2 = Diagnosis(diagnosis=pd[1])
         p = Participant(external_id='p1')
 
         # Add to participant and save
@@ -64,79 +73,82 @@ class ModelTest(FlaskTestCase):
 
         # Check Particpant has the diagnoses
         for d in Participant.query.first().diagnoses:
-            self.assertIn(d.primary_diagnosis, pd)
+            self.assertIn(d.diagnosis, pd)
 
         # Diagnoses have the participant
         p = Participant.query.first()
         for d in Diagnosis.query.all():
-            participants = [_p for _p in d.participants]
-            self.assertEqual(len(participants), 1)
-            self.assertEqual(participants[0].kf_id, p.kf_id)
+            self.assertEqual(d.participant_id, p.kf_id)
 
     def test_find_diagnosis(self):
         """
         Test find one diagnosis
         """
-        # Create diagnoses
+        # Create two diagnoses
         pd = ['cold', 'flu']
-        d1 = Diagnosis(primary_diagnosis=pd[0])
-        d2 = Diagnosis(primary_diagnosis=pd[1])
+        d1 = Diagnosis(diagnosis=pd[0])
+        d2 = Diagnosis(diagnosis=pd[1])
+        p = Participant(external_id='p1')
 
-        # Add and save to db
-        db.session.add(d1)
-        db.session.add(d2)
+        # Add to participant and save
+        p.diagnoses.extend([d1, d2])
+        db.session.add(p)
         db.session.commit()
 
         # Find diagnosis
         d = Diagnosis.query.\
-            filter_by(primary_diagnosis=pd[0]).one_or_none()
-        self.assertEqual(d.primary_diagnosis, pd[0])
+            filter_by(diagnosis=pd[0]).one_or_none()
+        self.assertEqual(d.diagnosis, pd[0])
 
     def test_update_diagnosis(self):
         """
         Test update diagnosis
         """
-        # Create diagnoses
+        # Create two diagnoses
         pd = ['cold', 'flu']
-        d1 = Diagnosis(primary_diagnosis=pd[0])
-        d2 = Diagnosis(primary_diagnosis=pd[1])
+        d1 = Diagnosis(diagnosis=pd[0])
+        d2 = Diagnosis(diagnosis=pd[1])
+        p = Participant(external_id='p1')
 
-        # Add and save to db
-        db.session.add(d1)
-        db.session.add(d2)
+        # Add to participant and save
+        p.diagnoses.extend([d1, d2])
+        db.session.add(p)
         db.session.commit()
 
         # Update and save
-        d = Diagnosis.query.filter_by(primary_diagnosis=pd[0]).one_or_none()
+        d = Diagnosis.query.filter_by(diagnosis=pd[0]).one_or_none()
         pr = 'yes'
-        d.progression_or_recurence = pr
+        d.progression_or_recurrence = pr
         db.session.commit()
 
         # Check updated values
-        d = Diagnosis.query.filter_by(primary_diagnosis=pd[0]).one_or_none()
-        self.assertEqual(d.progression_or_recurence, pr)
+        d = Diagnosis.query.filter_by(diagnosis=pd[0]).one_or_none()
+        self.assertEqual(d.progression_or_recurrence, pr)
 
     def test_delete_diagnosis(self):
         """
         Test delete diagnosis
         """
-        # Create diagnoses
+        # Create two diagnoses
         pd = ['cold', 'flu']
-        d1 = Diagnosis(primary_diagnosis=pd[0])
-        d2 = Diagnosis(primary_diagnosis=pd[1])
+        d1 = Diagnosis(diagnosis=pd[0])
+        d2 = Diagnosis(diagnosis=pd[1])
+        p = Participant(external_id='p1')
 
-        # Add and save to db
-        db.session.add(d1)
-        db.session.add(d2)
+        # Add to participant and save
+        p.diagnoses.extend([d1, d2])
+        db.session.add(p)
         db.session.commit()
 
         # Choose one and delete it
-        d = Diagnosis.query.filter_by(primary_diagnosis=pd[0]).one_or_none()
+        d = Diagnosis.query.filter_by(diagnosis=pd[0]).one_or_none()
         db.session.delete(d)
         db.session.commit()
 
-        d = Diagnosis.query.filter_by(primary_diagnosis=pd[0]).one_or_none()
+        d = Diagnosis.query.filter_by(diagnosis=pd[0]).one_or_none()
         self.assertIs(d, None)
+        diagnoses = [_d for _d in p.diagnoses]
+        self.assertNotIn(d, diagnoses)
 
     def test_delete_diagnosis_via_participant(self):
         """
@@ -144,33 +156,51 @@ class ModelTest(FlaskTestCase):
         """
         # Create two diagnoses
         pd = ['cold', 'flu']
-        d1 = Diagnosis(primary_diagnosis=pd[0])
-        d2 = Diagnosis(primary_diagnosis=pd[1])
+        d1 = Diagnosis(diagnosis=pd[0])
+        d2 = Diagnosis(diagnosis=pd[1])
+        p = Participant(external_id='p1')
 
-        # Create participants
-        p1 = Participant(external_id='p1')
-        p2 = Participant(external_id='p2')
-
-        # Add both diagnoses to first participant
-        p1.diagnoses.extend([d1, d2])
-        db.session.add(p1)
-
-        # Add one diagnoses to second participant and save
-        p2.diagnoses.append(d1)
-
+        # Add to participant and save
+        p.diagnoses.extend([d1, d2])
+        db.session.add(p)
         db.session.commit()
 
-        # Check that diagnosis 1 is part of both participants
-        d1 = Diagnosis.query.filter_by(primary_diagnosis=pd[0]).one()
-        d1_partipants = [p for p in d1.participants]
-        self.assertEqual(len(d1_partipants), 2)
-
-        # Delete first participant
-        db.session.delete(p1)
+        # Delete participant
+        db.session.delete(p)
         db.session.commit()
 
-        # Check that diagnosis 1 is part of only participant 2
-        d1 = Diagnosis.query.filter_by(primary_diagnosis=pd[0]).one()
-        d1_partipants = [p for p in d1.participants]
-        self.assertEqual(len(d1_partipants), 1)
-        self.assertEqual(d1_partipants[0].external_id, p2.external_id)
+        # Check that diagnoses have been deleted
+        d1 = Diagnosis.query.filter_by(diagnosis=pd[0]).one_or_none()
+        d2 = Diagnosis.query.filter_by(diagnosis=pd[1]).one_or_none()
+        self.assertIs(d1, None)
+        self.assertIs(d2, None)
+
+    def test_not_null_constraint(self):
+        """
+        Test that a diagnosis cannot be created without required
+        parameters such as participant_id
+        """
+        # Create diagnosis
+        data = {
+            'external_id': 'd1',
+            # non-existent required param: participant_id
+        }
+        d = Diagnosis(**data)
+
+        # Add to db
+        self.assertRaises(IntegrityError, db.session.add(d))
+
+    def test_foreign_key_constraint(self):
+        """
+        Test that a diagnosis cannot be created without an existing
+        reference Participant. This checks foreign key constraint
+        """
+        # Create diagnosis
+        data = {
+            'external_id': 'd1',
+            'participant_id': ''  # empty blank foreign key
+        }
+        d = Diagnosis(**data)
+
+        # Add to db
+        self.assertRaises(IntegrityError, db.session.add(d))
