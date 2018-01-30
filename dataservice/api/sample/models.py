7ff432a019bb620d50e0f26268c45933ae3b744a
Parimala Killada <killadap@email.chop.edu>
2018-01-30 11:39:11 -0500

    ✨ Aliquot model with Tests (#74)
     :sparkles: Add new fields to sample model with tests
     :sparkles: Add Aliquot model with tests

--- dataservice/api/sample/models.py
+++ dataservice/api/sample/models.py
@@ -1,5 +1,6 @@
 from dataservice.extensions import db
 from dataservice.api.common.model import Base
+from dataservice.api.aliquot.models import Aliquot
 
 
 class Sample(db.Model, Base):
@@ -8,10 +9,15 @@ class Sample(db.Model, Base):
     :param _id: Unique id assigned by RDBMS
     :param kf_id: Unique id given by the Kid's First DCC
     :param external_id: Name given to sample by contributor
-    :param composition : composition of the sample
-    :param tissue_type: Either Normal or Tumor
-    :param anatomical_site : location of the collected sample
-    :param age_at_event_days: age of participant when sample was collected
+    :param composition : The cellular composition of the sample.
+    :param tissue_type: description of the kind of tissue collected
+           with respect to disease status or proximity to tumor tissue
+    :param anatomical_site : The name of the primary disease site of the
+           submitted tumor sample
+    :param age_at_event_days: Age at the time sample was
+            acquired, expressed in number of days since birth
+    :param tumor_descriptor: The kind of disease present in the tumor
+           specimen as related to a specific timepoint
     """
     __tablename__ = "sample"
     external_id = db.Column(db.Text())
@@ -19,5 +25,9 @@ class Sample(db.Model, Base):
     composition = db.Column(db.Text())
     anatomical_site = db.Column(db.Text())
     age_at_event_days = db.Column(db.Integer())
-    participant_id = db.Column(db.Integer, db.ForeignKey('participant.kf_id'),
+    tumor_descriptor = db.Column(db.Text())
+    aliquots = db.relationship(Aliquot, backref='samples',
+                               cascade="all, delete-orphan")
+    participant_id = db.Column(db.String(8),
+                               db.ForeignKey('participant.kf_id'),
                                nullable=False)
