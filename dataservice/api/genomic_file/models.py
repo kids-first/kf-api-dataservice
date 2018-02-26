4e15574550aefe1be5611f1a05500b62ff67a006
Natasha Singh <dukedesi22@gmail.com>
2018-02-26 16:56:46 -0500

    :wrench: Add file_size to model class

--- dataservice/api/genomic_file/models.py
+++ dataservice/api/genomic_file/models.py
@@ -15,6 +15,7 @@ class GenomicFile(db.Model, Base):
     :param file_name: Name of file
     :param data_type: Type of genomic file (i.e. aligned reads)
     :param file_format: Format of file
+    :param file_size: Size of file in bytes
     :param file_url: Location of file
     :param md5sum: 128 bit md5 hash of file
     :param controlled_access: whether or not the file is controlled access
@@ -25,6 +26,7 @@ class GenomicFile(db.Model, Base):
     file_name = db.Column(db.Text())
     data_type = db.Column(db.Text())
     file_format = db.Column(db.Text())
+    file_size = db.Column(db.BigInteger())
     file_url = db.Column(db.Text())
     # See link for why md5sum should use uuid type
     # https://dba.stackexchange.com/questions/115271/what-is-the-optimal-data-type-for-an-md5-field
