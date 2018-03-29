bc2e938289d815eb87da9e1c43c062024955c38e
parimalak <parimala.killada@rockets.utoledo.edu>
2018-03-29 18:41:24 -0400

    :sparkles: Add release_status to study

--- dataservice/api/study/models.py
+++ dataservice/api/study/models.py
@@ -17,6 +17,7 @@ class Study(db.Model, Base):
     :param version: dbGaP version
     :param name: Name or title of study
     :param attribution: Link to attribution prose provided by dbGaP
+    :param release_status: Release status of the study
     """
     __tablename__ = 'study'
     __prefix__ = 'SD'
@@ -29,6 +30,7 @@ class Study(db.Model, Base):
     version = db.Column(db.Text())
     name = db.Column(db.Text())
     attribution = db.Column(db.Text())
+    release_status = db.Column(db.Text())
 
     participants = db.relationship(Participant,
                                    cascade="all, delete-orphan",
