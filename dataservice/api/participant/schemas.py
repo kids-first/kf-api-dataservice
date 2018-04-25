d175ac1a1e182dd191eb0e2f31f761d986f78621
Natasha Singh <dukedesi22@gmail.com>
2018-04-25 12:18:46 -0400

    :recycle: Refactor particip and study links to use custom URLFor

--- dataservice/api/participant/schemas.py
+++ dataservice/api/participant/schemas.py
@@ -4,6 +4,8 @@ from dataservice.api.participant.models import Participant
 from dataservice.api.common.schemas import BaseSchema
 from dataservice.extensions import ma
 
+from dataservice.api.common.custom_fields import PatchedURLFor
+
 
 class ParticipantSchema(BaseSchema):
     study_id = field_for(Participant, 'study_id', required=True,
@@ -20,29 +22,6 @@ class ParticipantSchema(BaseSchema):
     _links = ma.Hyperlinks({
         'self': ma.URLFor(Meta.resource_url, kf_id='<kf_id>'),
         'collection': ma.URLFor(Meta.collection_url),
-        'study': ma.URLFor('api.studies', kf_id='<study_id>')
+        'study': ma.URLFor('api.studies', kf_id='<study_id>'),
+        'family': PatchedURLFor('api.families', kf_id='<family_id>')
     })
-
-    def dump(self, obj, *args, **kwargs):
-        """
-        Modify schema dump to render nullable foreign keys as hyperlinks
-
-        For non-null foreign keys render as hyperlink in _links
-        For null foreign keys render as null in _links
-
-        Example after rendering links:
-        '_links': {
-            'self': /participants/PT_00001111,
-            'collection': /participants
-            'family': null,
-            'study': /studies/ST_00001111
-        }
-        """
-
-        marshal_result = super().dump(obj, *args, **kwargs)
-
-        self.render_nullable_fk_as_link('family_id',
-                                        'api.families',
-                                        marshal_result.data)
-
-        return marshal_result
--- dataservice/api/study/schemas.py
+++ dataservice/api/study/schemas.py
@@ -2,6 +2,7 @@ from marshmallow_sqlalchemy import field_for
 
 from dataservice.api.study.models import Study
 from dataservice.api.common.schemas import BaseSchema
+from dataservice.api.common.custom_fields import PatchedURLFor
 from dataservice.extensions import ma
 
 
@@ -17,29 +18,7 @@ class StudySchema(BaseSchema):
 
     _links = ma.Hyperlinks({
         'self': ma.URLFor(Meta.resource_url, kf_id='<kf_id>'),
-        'collection': ma.URLFor(Meta.collection_url)
+        'collection': ma.URLFor(Meta.collection_url),
+        'investigator': PatchedURLFor('api.investigators',
+                                      kf_id='<investigator_id>')
     })
-
-    def dump(self, obj, *args, **kwargs):
-        """
-        Modify schema dump to render nullable foreign keys as hyperlinks
-
-        For non-null foreign keys render as hyperlink in _links
-        For null foreign keys render as null in _links
-
-        Example after rendering links:
-        '_links': {
-            'self': /studies/ST_00001111,
-            'collection': /studies
-            'investigator': null,
-            'study': /studies/ST_00001111
-        }
-        """
-
-        marshal_result = super().dump(obj, *args, **kwargs)
-
-        self.render_nullable_fk_as_link('investigator_id',
-                                        'api.investigators',
-                                        marshal_result.data)
-
-        return marshal_result
