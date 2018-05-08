6ad4ea992fb021bc0ee2d8774ba156cf1f51ca74
Natasha Singh <dukedesi22@gmail.com>
2018-05-08 16:18:46 -0400

    :recycle: Make validate kf_id standalone validation method

--- dataservice/api/common/validation.py
+++ dataservice/api/common/validation.py
@@ -1,3 +1,4 @@
+import re
 from marshmallow import ValidationError
 
 MIN_AGE_DAYS = 0
@@ -28,3 +29,10 @@ def validate_positive_number(value):
     type_str = type(value).__name__
     if int(value) < 0:
         raise ValidationError('Must be a positive {}'.format(type_str))
+
+
+def validate_kf_id(prefix, value):
+    r = r'^' + prefix + r'_[A-HJ-KM-NP-TV-Z0-9]{8}'
+    m = re.search(r, value)
+    if not m:
+        raise ValidationError('Invalid kf_id')
