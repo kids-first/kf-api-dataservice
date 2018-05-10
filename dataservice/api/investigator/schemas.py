e1ea74af023aa29b4eb169fb222d25f429ed0d03
Natasha Singh <dukedesi22@gmail.com>
2018-05-10 12:41:12 -0400

    :recycle: :white_check_mark: Refactor investg links

--- dataservice/api/investigator/schemas.py
+++ dataservice/api/investigator/schemas.py
@@ -8,8 +8,10 @@ class InvestigatorSchema(BaseSchema):
         model = Investigator
         resource_url = 'api.investigators'
         collection_url = 'api.investigators_list'
+        exclude = BaseSchema.Meta.exclude + ('studies', )
 
     _links = ma.Hyperlinks({
         'self': ma.URLFor(Meta.resource_url, kf_id='<kf_id>'),
-        'collection': ma.URLFor(Meta.collection_url)
+        'collection': ma.URLFor(Meta.collection_url),
+        'studies': ma.URLFor('api.studies_list', investigator_id='<kf_id>')
     })
--- tests/test_api.py
+++ tests/test_api.py
@@ -203,7 +203,7 @@ class TestAPI:
 
     @pytest.mark.parametrize('endpoint,child_relations', [
         ('/studies', ['study_files', 'participants']),
-        # ('/investigators', ['studies']),
+        ('/investigators', ['studies']),
         # ('/families', ['participants']),
         # ('/sequencing-centers', ['sequencing_experiments', 'biospecimens']),
         # ('/participants', ['diagnoses',
