83aedb4ca9902b0537abba8e60e642885d56fbc8
Natasha Singh <dukedesi22@gmail.com>
2024-05-15 15:22:43 -0400

    :sparkles: Initial sample relationships validation schema

---
+++ dataservice/api/sample_relationship/schemas.py
@@ -0,0 +1,43 @@
+from marshmallow_sqlalchemy import field_for
+from marshmallow import (
+    fields,
+    validates
+)
+
+from dataservice.api.sample_relationship.models import SampleRelationship
+from dataservice.api.common.schemas import BaseSchema
+from dataservice.api.common.validation import validate_kf_id
+from dataservice.extensions import ma
+
+
+class SampleRelationshipSchema(BaseSchema):
+    parent_id = field_for(SampleRelationship, 'parent_id',
+                          required=True,
+                          load_only=True, example='SA_B048J5')
+    child_id = field_for(SampleRelationship, 'child_id',
+                         required=True,
+                         load_only=True, example='SA_B048J6')
+
+    class Meta(BaseSchema.Meta):
+        model = SampleRelationship
+        resource_url = 'api.sample_relationships'
+        collection_url = 'api.sample_relationships_list'
+        exclude = BaseSchema.Meta.exclude + ('child', 'parent')
+
+    _links = ma.Hyperlinks({
+        'self': ma.URLFor(Meta.resource_url, kf_id='<kf_id>'),
+        'collection': ma.URLFor(Meta.collection_url),
+        'parent': ma.URLFor('api.samples',
+                            kf_id='<parent_id>'),
+        'child': ma.URLFor('api.samples',
+                           kf_id='<child_id>')
+    })
+
+
+class SampleRelationshipFilterSchema(SampleRelationshipSchema):
+
+    sample_id = fields.Str()
+
+    @validates('sample_id')
+    def valid_sample_id(self, value):
+        validate_kf_id('SA', value)
