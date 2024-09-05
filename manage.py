d43d18d43ee3b30e601b9bc899fc9ae88de81a80
Natasha Singh <dukedesi22@gmail.com>
2024-09-05 11:26:34 -0400

    :recycle: Make docker container listen on all interfaces
    See https://www.reddit.com/r/docker/comments/xwfm08/why_do_i_need_to_specify_host0000_when_running_a/

--- manage.py
+++ manage.py
@@ -5,5 +5,10 @@ from dataservice import create_app
 
 app = create_app(os.environ.get('FLASK_CONFIG') or 'default')
 
+BIND_ALL_INTERFACES = os.environ.get("BIND_ALL_INTERFACES")
+host = None
+if BIND_ALL_INTERFACES == "enabled":
+    host = "0.0.0.0"
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host=host)
